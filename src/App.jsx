4f0d4d67a86517251ce1bdbf574434a42761d35f
Shehan Mallawaarchchi <shehan96mts@gmail.com>
2022-09-26 15:00:07 +0530

    Initial commi

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import bulbOn from './assets/Images/bulb_on.png'
+import bulbOff from './assets/Images/bulb_off.png'
+
+function App() {
+//buttion ofF  image is default state 
+  const [img, setImg] = useState(bulbOff);
+//change state thrugh the  buttion 
+  return ( 
+    <div>
+     <h1>Task 1  #</h1>
+     <button onClick={()=>setImg(bulbOn)}> On the bulb </button>
+     <img src={img} style={{width:"250px" ,height:"200px"}} />
+    <button onClick={()=>setImg(bulbOff)}>Off the bulb</button>
+    </div>
+  
+   );
+
+}
+
+
+export default App;
---
+++ src/componets/home.jsx
@@ -0,0 +1,12 @@
+function home() {
+    return ( 
+        <div>
+            <h1>Task 1 </h1>
+
+
+            <img src={require('../assets/Images/bulb_off.png')} />
+        </div>
+     );
+}
+
+export default home;
